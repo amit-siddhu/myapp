336ecbc4c6c576ea1043d09ccb7de231b6866b36
Amit Siddhu <amit.siddhu7@gmail.com>
2013-04-21 22:10:16 +0530

    21/04/2013

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,12 +9,19 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'My App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
+                        :text => "Amit Siddhu")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
+
+  let(:base_title) { "Amit Siddhu" }
 
   describe "Help page" do
 
@@ -25,8 +32,7 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+      page.should have_selector('title', :text => "#{base_title} | Help")
     end
   end
 
@@ -39,9 +45,8 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      page.should have_selector('title',
-                    :text => "Ruby on Rails Tutorial Sample App | About Us")
+      page.should have_selector('title', :text => "#{base_title} | About Us")
     end
   end
-end
 
+end
